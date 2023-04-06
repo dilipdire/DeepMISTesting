5ff7d75a529fe94bda06ee053a45533bdb261782
Dilip <aniketdire4@gmail.com>
2023-04-06 15:40:27 +0530

    print duplicate number

---
+++ src/test/java/mis/PrintDuplicate.java
@@ -0,0 +1,24 @@
+package mis;
+
+import java.util.HashMap;
+
+public class PrintDuplicate {
+
+	public static void main(String[] args) {
+		String str = "dilipii";
+		int count = 0;
+
+		HashMap<Character, Integer> hm = new HashMap<Character, Integer>();
+		for (int i = 0; i < str.length(); i++) {
+			if (!hm.containsKey(str.charAt(i))) {
+				hm.put(str.charAt(i), count);
+
+			} else {
+				hm.put(str.charAt(i), hm.get(str.charAt(i)) + 1);
+			}
+		}
+		System.out.print(hm);
+
+	}
+
+}
