d80fa7312a8369b02c0fff1e7ef6a029f869f9c3
Dilip <aniketdire4@gmail.com>
2023-04-07 23:22:04 +0530

    Add method

--- src/test/java/mis/EnumDemo.java
+++ src/test/java/mis/EnumDemo.java
@@ -8,6 +8,12 @@ public class EnumDemo {
 	     code,
 	      mcq,
 	}
+	
+	public enum Color{
+		
+		RED,GREEN,YOLLOW,BLUE,BLACK,WHITE;
+		
+	}
 
 	public static void main(String[] args) {
 		
@@ -18,6 +24,24 @@ public class EnumDemo {
 		System.out.println("Size : "+tst.size());
 		System.out.println("Code :"+tst.get(Test.code));
 		System.out.println("MCQ  :"+tst.get(Test.mcq));
+		
+		demoColor();
+		
+	}
+	
+	public static void demoColor()
+	{
+		EnumMap<Color,Integer> color = new EnumMap<Color,Integer>(Color.class);
+		color.put(Color.RED, 1);
+		color.put(Color.GREEN, 2);
+		color.put(Color.YOLLOW, 3);
+		color.put(Color.BLUE, 4);
+		color.put(Color.BLACK, 5);
+		color.put(Color.WHITE, 6);
+		
+		System.out.print("ENTRYSET: "+color.entrySet()+"\n KEYSET:"+color.keySet()+" \n Value:"+color.values());
+		
+		
 		
 	}
 }
